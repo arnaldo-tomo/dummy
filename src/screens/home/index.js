ef11b5d486a517009c86fb16d822b58ed72c55e6
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-22 04:34:49 +0200

    Create index.js

---
+++ src/screens/home/index.js
@@ -0,0 +1,10 @@
+import { Text } from "react-native"
+import { View } from "react-native"
+
+export const Home = () => {
+    return (
+        <View>
+            <Text>HOME</Text>
+        </View>
+    )
+}
