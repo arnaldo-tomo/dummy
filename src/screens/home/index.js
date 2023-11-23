0464831c14c9d67b9e38609aac7e3a4d7624e979
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-23 12:39:44 +0200

    Update index.js

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,10 +1,19 @@
-import { Text } from "react-native"
+import { Text, TouchableOpacity } from "react-native"
 import { View } from "react-native"
+import { Services } from "../../../services"
 
+export const OutSide = ({ navigation }) => {
+    Services.removeValue();
+    navigation.navigate('OneBoard');
+}
 export const Home = () => {
     return (
         <View>
             <Text>HOME</Text>
+
+            <TouchableOpacity onPress={() => OutSide()}>
+                <Text>Logout</Text>
+            </TouchableOpacity>
         </View>
     )
 }
