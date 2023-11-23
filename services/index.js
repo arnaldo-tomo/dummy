d21bc0384a78bc004e3b1963a42ec2896c2e0abe
Arnaldo tomo <arnaldotomo@gmail.com>
2023-11-23 15:25:22 +0200

    Update index.js

--- services/index.js
+++ services/index.js
@@ -1,8 +1,9 @@
 import { Alert } from "react-native";
-
 import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 export class Services {
-    static async AuthUser(email, password) {
+    static async AuthUser({ email, password, navigation }) {
 
         try {
             const auth = await fetch('https://dummyjson.com/auth/login', {
@@ -17,7 +18,8 @@ export class Services {
                 .then((p) => {
                     if (p.token) {
                         console.log(p)
-                        this.storeData(p);
+                        this.storeData(p)
+                        this.GoHome({ navigation })
                     }
                     if (p.message) {
                         Alert.alert(p.message)
@@ -25,17 +27,16 @@ export class Services {
                 })
                 .catch((error) => console.log('catch no login', error))
         } catch (error) {
-            console.log('CAtch', error)
+            console.log('Catch', error)
         }
     }
+
     static storeData = async (p) => {
         try {
-
             const jsonValue = JSON.stringify(p);
             await AsyncStorage.setItem('UserToken', jsonValue);
 
         } catch (e) {
-            // saving error
             console.log(e)
         }
     };
@@ -43,7 +44,6 @@ export class Services {
     static getData = async () => {
         try {
             const jsonValue = await AsyncStorage.getItem('UserToken');
-            // return jsonValue != null ? JSON.parse(jsonValue) : null;
             if (jsonValue) {
                 console.log('getData', jsonValue)
                 return true;
@@ -67,7 +67,15 @@ export class Services {
     }
 
 
-}
+    static GoHome({ navigation }) {
+        navigation.reset({ index: 0, routes: [{ name: 'Home' }] });
+    }
+    static GoOut({ navigation }) {
+        this.removeValue();
+        navigation.reset({ index: 0, routes: [{ name: 'Login' }] });
+    }
 
+
+}
 
 
